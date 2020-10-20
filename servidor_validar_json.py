1b9d9e95ebfbbab01d1ed7a03c8f399fa99fc09f
silvaliliaa <61975362+silvaliliaa@users.noreply.github.com>
2020-10-20 19:09:29 -0300

    Update servidor_validar_json.py

--- servidor_validar_json.py
+++ servidor_validar_json.py
@@ -12,13 +12,33 @@ def validar_json(dados):
             # Em caso contrário, retornar 400.
             resposta = {"Erro": "Campo 'medida' deve ser string."}
             return resposta, 400
-
+       
+    url_de_teste = None
+    if "marcadores" not in dados:
+        resposta = {"Erro": "Campo 'marcadores' inexistente."}
+        return resposta, 400
+    else:
+        for item in dados["marcadores"]:
+            for chave, valor in item.items():
+                if chave.lower() == "url":
+                    url_de_teste = valor
+        if url_de_teste:
+            try:
+                req = get(url_de_teste)
+                if req.status_code >= 400:
+                    resposta = {"Erro": "URL não permitida."}
+                    return resposta, 400
+            except:
+                resposta = {"Erro": "URL incompleta. Possível problema de resolução DNS."}
+                return resposta, 400
     # Verificar se a chave 'valores' consta no JSON.
+ 
     if "valores" not in dados:
         # Em caso contrário, retornar 400.
         resposta = {"Erro": "Campo 'valores' inexistente."}
         return resposta, 400
     else:
+        
         # 'valores' existe, agora é preciso verificar se existe pelo menos
         # 1 par chave-valor.
         if type(dados["valores"]) is not list or len(dados["valores"]) < 1:
@@ -26,20 +46,7 @@ def validar_json(dados):
             resposta = {
                 "Erro": "Campo 'valores' deve ser uma lista não vazia."}
             return resposta, 400
-if "marcadores" not in dados:
-        resposta = {"Erro": "Campo 'marcadores' inexistente."}
-        return resposta,400
-    else:
-        for item in dados["marcadores"]:
-            for chave, valor in item.itens():
-                if chave.lower() == "url":
-                    url_de_teste = valor
-                    
-        if url_de_teste:
-            req = get(url_de_teste)
-            if req.status_code >= 200 and req.status_code <= 399:
-                resposta = {"Sucesso": "URL válida"}
-                return resposta, 200
+
     # Se todas as verificações foram feitas, retornar 200.
     resposta = {"Sucesso": "JSON válido."}
     return resposta, 200
